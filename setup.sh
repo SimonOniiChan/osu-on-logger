2811405305f789322cee3a670f9d6f821e36571b
gamescom15 <gamescom15@users.noreply.github.com>
2017-04-18 13:39:54 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,4 +3,4 @@ echo -n "Please specify the users you want to track with their id. The name will
 read users
 echo $users >> $(dirname $(readlink -f "$0"))/ids.config
 mkdir -p log 
-refresh.bash
+bash "$(dirname $(readlink -f "$0"))/refresh.bash"
