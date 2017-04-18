bf6377b8d8a98de2e87c7fe4b0291c822dc3a274
gamescom15 <gamescom15@users.noreply.github.com>
2017-04-18 14:07:35 +0200

    Update run-as-root.bash
    Added Comments

--- run-as-root.bash
+++ run-as-root.bash
@@ -1,9 +1,9 @@
 #/bin/bash
 
 echo trying to add cronjob...
-if [ "$EUID" -ne 0 ]
+if [ "$EUID" -ne 0 ] # Check if running as root
   then echo "Please run as root, as it it required for adding global cronjobs"
   exit
 fi
-echo "*/10 *	* * *	root	bash $(dirname $(readlink -f "$0"))/refresh.bash" >> /etc/crontab
-chmod +x $(dirname $(readlink -f "$0"))/refresh.bash
+echo "*/10 *	* * *	root	bash $(dirname $(readlink -f "$0"))/refresh.bash" >> /etc/crontab # add the crontab entry (adjust the root to your username if you feel the need to)
+chmod +x $(dirname $(readlink -f "$0"))/refresh.bash # allow cron to execute the script 
