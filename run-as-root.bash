589769e702e2d1fcbf404ec1e6157ca18870c809
gamescom15 <gamescom15@users.noreply.github.com>
2017-04-18 13:44:17 +0200

    Update run-as-root.bash

--- run-as-root.bash
+++ run-as-root.bash
@@ -5,5 +5,5 @@ if [ "$EUID" -ne 0 ]
   then echo "Please run as root, as it it required for adding global cronjobs"
   exit
 fi
-echo "*/10 *	* * *	root	sh $(dirname $(readlink -f "$0"))/refresh.bash" >> /etc/crontab
-chmod +x $(dirname $(readlink -f "$0"))/*.*
+echo "*/10 *	* * *	root	bash $(dirname $(readlink -f "$0"))/refresh.bash" >> /etc/crontab
+chmod +x $(dirname $(readlink -f "$0"))/refresh.bash
