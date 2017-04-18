5d0a748586ef11f68acfd57d56018a4763482d35
gamescom15 <gamescom15@users.noreply.github.com>
2017-04-18 14:08:37 +0200

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 #/bin/sh
 echo -n "Please specify the users you want to track with their id. The name will work most of the time too but may lead to bugs.\n-->"
-read users
+read users # read what to put in the config file
 echo $users >> $(dirname $(readlink -f "$0"))/ids.config
-mkdir -p log 
-bash "$(dirname $(readlink -f "$0"))/refresh.bash"
+mkdir -p log # create the log folder
+bash "$(dirname $(readlink -f "$0"))/refresh.bash" # run the script once
